1036d247580f28ed303547d868602aa68d61711b
tsainis.k93@gmail.com <tsainis.k93@gmail.com>
2022-01-18 00:40:30 +0000

    added apiURl

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://7209b3ca.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
